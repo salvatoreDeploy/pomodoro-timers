e3588a29b40016e9bc917e60bfe37eb75c0e922d
salvatoreDeploy <liderhenrique@gmail.com>
2023-12-14 17:55:42 -0300

    Reducers - Utilizando immer

--- Readme.md
+++ Readme.md
@@ -15,3 +15,4 @@
 - Zod
 - @hookform/resolvers
 - Date-fns
+- Immer
--- src/reducers/cyclesTask/reducer.ts
+++ src/reducers/cyclesTask/reducer.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { ActionTypes } from './actions'
+import { produce } from 'immer'
 
 export interface CycleTask {
   id: string
@@ -20,13 +21,19 @@ interface CycleTaskState {
 export function cycleTaskReducer(state: CycleTaskState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLETASK:
-      return {
+      /* return {
         ...state,
         cycleTask: [...state.cycleTask, action.payload.newCycleTask],
         activeCycleTaskId: action.payload.newCycleTask.id,
-      }
-    case ActionTypes.ITERRUPT_CURRENT_CYCLETASK:
-      return {
+      } */
+
+      return produce(state, (draft) => {
+        draft.cycleTask.push(action.payload.newCycleTask)
+        draft.activeCycleTaskId = action.payload.newCycleTask.id
+      })
+
+    case ActionTypes.ITERRUPT_CURRENT_CYCLETASK: {
+      /* return {
         ...state,
         cycleTask: state.cycleTask.map((cycle) => {
           if (cycle.id === state.activeCycleTaskId) {
@@ -36,9 +43,24 @@ export function cycleTaskReducer(state: CycleTaskState, action: any) {
           }
         }),
         activeCycleTaskId: null,
+      } */
+
+      const currentCycleTaskIndex = state.cycleTask.findIndex((cycle) => {
+        return cycle.id === state.activeCycleTaskId
+      })
+
+      if (currentCycleTaskIndex < 0) {
+        return state
       }
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
+
+      return produce(state, (draft) => {
+        draft.cycleTask[currentCycleTaskIndex].interruptedDate = new Date()
+        draft.activeCycleTaskId = null
+      })
+    }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      /* return {
         ...state,
         cycleTask: state.cycleTask.map((cycle) => {
           if (cycle.id === state.activeCycleTaskId) {
@@ -48,7 +70,22 @@ export function cycleTaskReducer(state: CycleTaskState, action: any) {
           }
         }),
         activeCycleTaskId: null,
+      } */
+
+      const currentCycleTaskIndex = state.cycleTask.findIndex((cycle) => {
+        return cycle.id === state.activeCycleTaskId
+      })
+
+      if (currentCycleTaskIndex < 0) {
+        return state
       }
+
+      return produce(state, (draft) => {
+        draft.cycleTask[currentCycleTaskIndex].finishedDate = new Date()
+        draft.activeCycleTaskId = null
+      })
+    }
+
     default:
       return state
   }
